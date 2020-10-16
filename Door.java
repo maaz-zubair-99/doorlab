ff65d975baf501cdbe8b7e9554577ba36497d12f
Maaz Zubair <mzubair@gmail.com>
2020-10-16 13:05:11 -0500

    Initial sharing of project

---
+++ Door.java
@@ -0,0 +1,26 @@
+
+public class Door
+{
+    private String name;
+    private boolean isOpened;
+    public Door(String name,boolean isOpened){
+        this.name=name;
+        this.isOpened = isOpened;
+    }
+    public void close(){
+        this.isOpened = false;
+    }
+    public void open(){
+        this.isOpened = true;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public boolean getOpen(){
+        return this.isOpened;
+    }
+    public void setName(String newName)
+    {
+       this.name = newName;
+    }
+}
